84135383f657126eb018fc6e8ee1e3298ba8fbbc
thecodecrusher12 <popikontova21@gmail.com>
2025-03-09 11:13:33 +0100

    changes

---
+++ Content.java
@@ -0,0 +1,21 @@
+package backend.repository;
+
+public class Content {
+    private String title, primaryGenre;
+    private int contentId = 0;
+    private double avgRating;
+    private float[] ratings;
+    private String[] actorNames;
+    private String director;
+    private int ratingCoount;
+    private int MAX_RATINGS;
+    private int MAX_ACTORS;
+
+    Content(String title, String genre, String director){
+//
+    }
+    Content(String title){
+
+    }
+}
+
